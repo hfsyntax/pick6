1f41e2e2e00563c2ff70886a372fcc31b9314c4f
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-10-17 19:58:16 -0400

    Update process_seasons.bat
    convert comments to output and only create zip if no errors

--- resources/process_seasons.bat
+++ resources/process_seasons.bat
@@ -5,18 +5,19 @@ set "batch_dir=%~dp0"
 set "output_folder=%batch_dir%seasons"
 set "destination_zip=%batch_dir%\seasons.zip" 
 
-REM Remove existing "output" folder if it exists
 if exist "%output_folder%" (
+    echo deleted previous output folder
     rmdir /s /q "%output_folder%"
 )
 
 mkdir "%output_folder%"
+echo created the output folder
 
 pushd "%batch_dir%"
 
-REM Remove existing zip file if it exists
 if exist "%destination_zip%" (
     del "%destination_zip%"
+    echo deleted previous seasons zip
 )
 
 for /d %%s in (*) do (
@@ -25,7 +26,6 @@ for /d %%s in (*) do (
     for /d %%w in (*) do (
         pushd "%%w"
 
-        REM Loop through files containing "Histo" in their names
         for %%f in (*Histo*) do (
             set "season_folder=%%s"
   
@@ -38,11 +38,10 @@ for /d %%s in (*) do (
 
             set "picks_file=Week!week_folder!_!season_folder!_Picks_!file_extension!"
 	    set "games_file=Week!week_folder!_!season_folder!_Games_!file_extension!"
-			            
-	   !batch_dir!XlsToCsv "%%~dpnxf" "Picks" "%output_folder%\!picks_file!"
-	   echo created "%output_folder%\!picks_file!"
-	   !batch_dir!XlsToCsv "%%~dpnxf" "Games" "%output_folder%\!games_file!"
-	   echo created "%output_folder%\!games_file!"
+            !batch_dir!XlsToCsv "%%~dpnxf" "Picks" "%output_folder%\!picks_file!"
+	    echo created "%output_folder%\!picks_file!"
+	    !batch_dir!XlsToCsv "%%~dpnxf" "Games" "%output_folder%\!games_file!"
+	    echo created "%output_folder%\!games_file!"
         )
 
         popd
@@ -51,12 +50,16 @@ for /d %%s in (*) do (
     popd
 )
 
-REM Create a new zip file in the current directory
 powershell Compress-Archive -Path "%output_folder%\*" -DestinationPath "%destination_zip%"
+set powershell_exit_code=%errorlevel%
 
-REM Delete the "output" folder
-rmdir /s /q "%output_folder%"
+if %powershell_exit_code% equ 0 (
+    rmdir /s /q "%output_folder%"
+    echo Zip file created successfully at %destination_zip%
+) else (
+    echo Compression failed with error code %powershell_exit_code%.
+)
 
 popd
 
-echo Zip file created successfully at %destination_zip%
+pause
