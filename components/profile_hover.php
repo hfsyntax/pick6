7420aa6c45789a0b2cbef612b3cac59c887a43dd
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-10-18 03:24:04 -0400

    Update profile_hover.php
    display navbar component for pages where user is not logged in

--- components/profile_hover.php
+++ components/profile_hover.php
@@ -1,10 +1,10 @@
-<?php if (isset($_SESSION["user"])) : ?>
 <div class="profile-hover">
     <div class="user-profile-icon fa fa-user-circle"></div>
     <div class="user-profile">
+    <?php if (isset($_SESSION["user"])) : ?>
         <span class=""><?php echo $_SESSION["user"]; ?></span></br>
         <span class=""><?php echo $_SESSION["type"]; ?></span>
         <h1 id="logoff">LogOut</h1> 	
     </div>
+    <?php endif; ?>
 </div>
-<?php endif; ?>
