b24d5fe4d0f3ed0e97ef3698e15503a381666d3b
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-10-18 03:26:14 -0400

    Update topbar.php
    display navbar component for users not logged in

--- components/topbar.php
+++ components/topbar.php
@@ -1,13 +1,15 @@
-<?php if (isset($_SESSION["user"])) : ?>
 <header class="main-header col-lg-12">
     <span class="logo-mid"><img src="components/p6-logo.png"/>Pick6</span>
+    <?php if (isset($_SESSION["user"])) : ?>
     <div class="fa fa-gears" id="settings_bar"></div>
     <div class="user-profile">
-    <span><?php echo $_SESSION["user"]; ?></br><?php echo $_SESSION["type"]; ?></span>
+        <span>
+            <?php echo $_SESSION["user"]; ?></br><?php echo $_SESSION["type"]; ?>
+        </span>
         <div class="mini-user fa fa-user-circle"></div>
     </div>
     <div class="fa fa-envelope notification-icon" id="icon_bar">
         <span class="label bg-orange">0</span> 
     </div>
+    <?php endif; ?>
 </header>
-<?php endif; ?>
